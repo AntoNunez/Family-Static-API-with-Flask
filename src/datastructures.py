46bfa28f3e290970c6a59705fbe2f33c6b57d4c5
AntoNunez <a.nunez.jeria@gmail.com>
2022-08-08 19:35:29 +0000

    Family Static API with Flask

--- src/datastructures.py
+++ src/datastructures.py
@@ -8,12 +8,35 @@ update this file to implement the following already declared methods:
 """
 from random import randint
 
+
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [
+            {
+            "id": self._generateId(),
+            "first_name": "John",
+            "last_name": self.last_name,
+            "age": 33,
+            "lucky_numbers": [7, 13, 22]
+        },
+            {
+            "id": self._generateId(),
+            "first_name": "Jane",
+            "last_name": self.last_name,
+            "age": 35,
+            "lucky_numbers": [10, 14, 3]
+        },
+            {
+            "id": self._generateId(),
+            "first_name": "Jimmy",
+            "last_name": self.last_name,
+            "age": 5,
+            "lucky_numbers": [1]
+
+        }]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
@@ -21,16 +44,33 @@ class FamilyStructure:
 
     def add_member(self, member):
         # fill this method and update the return
-        pass
+        if member['id'] is None: member["id"]=self._generateId() 
+        member["last_name"] = self.last_name
+        self._members.append(member)
+        return member
 
-    def delete_member(self, id):
-        # fill this method and update the return
-        pass
 
     def get_member(self, id):
         # fill this method and update the return
-        pass
+        for position in range(len(self._members)):
+            if self._members[position]["id"] == int(id):
+                return self._members[position]
+        return None
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
+
+    def update_member(self, id, member):
+        for position in range(len(self._members)):
+            if self._members[position]["id"] == int(id):
+                self._members[position].update(member)
+                return {"complete,member updated": True}  
+   
+    def delete_member(self, id):
+        # fill this method and update the return
+        for position in range(len(self._members)):
+            if self._members[position]["id"] == int(id):
+                self._members.pop(position)
+                return {"Completed, member removed": True}
+        return None
